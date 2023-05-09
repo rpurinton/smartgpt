9f1aef92fa8ad165117c2fefcd984dfc6949bc9d
Russell Purinton <russell.purinton@gmail.com>
2023-05-09 05:29:22 -0400

    Update SmartGPT.php

--- src/SmartGPT.php
+++ src/SmartGPT.php
@@ -9,22 +9,34 @@ class SmartGPT
 		echo ("Prompt: $prompt\n");
 		require_once(__DIR__ . "/BunnyAI.php");
 		$bunnyai = new BunnyAI;
-		$messages[] = ["role" => "user", "content" => "User Input: \"$prompt\""];
-		$messages[] = ["role" => "user", "content" => "
+		$base_input = ["role" => "user", "content" => "User Input: \"$prompt\""];
+		$generate_guides = ["role" => "user", "content" => "
 		Generate a step-by-step guide that encourages the assistant to think creatively and 
 		come up with an innovative solution to formulating a response to the User input,
 		without being able to request any additional User Input.
 		The guide should be open-ended and allow for multiple interpretations and approaches.
 		Give just the final steps, without any examples, explanations, or cognitive distortions."];
-		for ($i = 0; $i < 16; $i++) {
-			$messagess[] = $messages;
-		}
+		$messages = [];
+		$messages[] = $base_input;
+		$messages[] = $generate_guides;
+		for ($i = 0; $i < 16; $i++) $messagess[] = $messages;
+		echo ("Generating Guides...(0/16)...");
 		$responses = $bunnyai->get($bunnyai->build_prompts($messagess));
+		$response_count = 0;
+		$response_total = count($responses);
 		foreach ($responses as $response) {
-			$this->usage['promptTokens'] += $response['response']['usage']['promptTokens'];
-			$this->usage['completionTokens'] += $response['response']['usage']['completionTokens'];
-			$this->usage['totalTokens'] += $response['response']['usage']['totalTokens'];
+			$response_count++;
+			if (isset($response['response']) && isset($response['response']['usage'])) {
+				if (isset($response['response']['usage']['promptTokens']))
+					$this->usage['promptTokens'] += $response['response']['usage']['promptTokens'];
+				if (isset($response['response']['usage']['completionTokens']))
+					$this->usage['completionTokens'] += $response['response']['usage']['completionTokens'];
+				if (isset($response['response']['usage']['totalTokens']))
+					$this->usage['totalTokens'] += $response['response']['usage']['totalTokens'];
+			}
+			echo ("\rGenerating Guides...($response_count/$response_total)...");
 		}
+		echo ("done.\n");
 		print_r($responses);
 		print_r($this->usage);
 	}
