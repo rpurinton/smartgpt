3f0be9c05eabf2cd1e68697b2c6617c628052690
Russell Purinton <russell.purinton@gmail.com>
2023-05-09 07:26:40 -0400

    Update SmartGPT.php

--- src/SmartGPT.php
+++ src/SmartGPT.php
@@ -144,8 +144,139 @@ class SmartGPT
 			}
 			echo ("\rResolving Responses...($response_count/$response_total)...");
 		}
+		echo ("done.\n");
+		$base_messages = $messages;
+		$messages[] = ["role" => "user", "content" => "Imagine you are a devil's advocate who is tasked with critisizing these Possible Responses,\n" .
+			"Identify any errors, inconsistencies, nuances, caveats, edge cases not included, and/or cognitive distortions.\n" .
+			"Use your outside-the-box Critical Thinking skills.  Keep your response as short and concise as possible while still hitting all the important details."];
+		$messagess = [];
+		for ($i = 0; $i < 4; $i++) $messagess[] = $messages;
+		echo ("Playing Devil's Advocate...(0/4)...");
+		$responses = $bunnyai->get($bunnyai->build_prompts($messagess));
+		$response_count = 0;
+		$response_total = count($responses);
+		$messages = $base_messages;
+		foreach ($responses as $response) {
+			$response_count++;
+			if (isset($response['response'])) {
+				if (isset($response['response']['choices'])) {
+					foreach ($response['response']['choices'] as $choice) {
+						if (isset($choice['message']) && isset($choice['message']['content'])) {
+							$messages[] = ["role" => "user", "content" => "Devil's Advocate $response_count of $response_total: " . $choice['message']['content']];
+						}
+					}
+				}
+				if (isset($response['response']['usage'])) {
+					if (isset($response['response']['usage']['promptTokens']))
+						$this->usage['promptTokens'] += $response['response']['usage']['promptTokens'];
+					if (isset($response['response']['usage']['completionTokens']))
+						$this->usage['completionTokens'] += $response['response']['usage']['completionTokens'];
+					if (isset($response['response']['usage']['totalTokens']))
+						$this->usage['totalTokens'] += $response['response']['usage']['totalTokens'];
+				}
+			}
+			echo ("\rPlaying Devil's Advocate...($response_count/$response_total)...");
+		}
+		echo ("done.\n");
+		$messages[] = ["role" => "user", "content" => "Based on the User Input and Possible Responses, and while mitigating the issues identified by the Devil's Advocates,\n" .
+			"Use your outside-the-box Critical Thinking skills to come up with the most accurate and concise response.\n" .
+			"Give only your final response without any cognitive distortions."];
+		$messagess = [];
+		for ($i = 0; $i < 2; $i++) $messagess[] = $messages;
+		echo ("Resolving Response...(0/2)...");
+		$responses = $bunnyai->get($bunnyai->build_prompts($messagess));
+		$response_count = 0;
+		$response_total = count($responses);
+		$messages = $base_input;
+		foreach ($responses as $response) {
+			$response_count++;
+			if (isset($response['response'])) {
+				if (isset($response['response']['choices'])) {
+					foreach ($response['response']['choices'] as $choice) {
+						if (isset($choice['message']) && isset($choice['message']['content'])) {
+							$messages[] = ["role" => "user", "content" => "Possible Response $response_count of $response_total: " . $choice['message']['content']];
+						}
+					}
+				}
+				if (isset($response['response']['usage'])) {
+					if (isset($response['response']['usage']['promptTokens']))
+						$this->usage['promptTokens'] += $response['response']['usage']['promptTokens'];
+					if (isset($response['response']['usage']['completionTokens']))
+						$this->usage['completionTokens'] += $response['response']['usage']['completionTokens'];
+					if (isset($response['response']['usage']['totalTokens']))
+						$this->usage['totalTokens'] += $response['response']['usage']['totalTokens'];
+				}
+			}
+			echo ("\rResolving Responses...($response_count/$response_total)...");
+		}
+		echo ("done.\n");
+		$base_messages = $messages;
+		$messages[] = ["role" => "user", "content" => "Imagine you are a devil's advocate who is tasked with critisizing these Possible Responses,\n" .
+			"Identify any errors, inconsistencies, nuances, caveats, edge cases not included, and/or cognitive distortions.\n" .
+			"Use your outside-the-box Critical Thinking skills.  Keep your response as short and concise as possible while still hitting all the important details."];
+		$messagess = [];
+		for ($i = 0; $i < 4; $i++) $messagess[] = $messages;
+		echo ("Playing Devil's Advocate...(0/4)...");
+		$responses = $bunnyai->get($bunnyai->build_prompts($messagess));
+		$response_count = 0;
+		$response_total = count($responses);
+		$messages = $base_messages;
+		foreach ($responses as $response) {
+			$response_count++;
+			if (isset($response['response'])) {
+				if (isset($response['response']['choices'])) {
+					foreach ($response['response']['choices'] as $choice) {
+						if (isset($choice['message']) && isset($choice['message']['content'])) {
+							$messages[] = ["role" => "user", "content" => "Devil's Advocate $response_count of $response_total: " . $choice['message']['content']];
+						}
+					}
+				}
+				if (isset($response['response']['usage'])) {
+					if (isset($response['response']['usage']['promptTokens']))
+						$this->usage['promptTokens'] += $response['response']['usage']['promptTokens'];
+					if (isset($response['response']['usage']['completionTokens']))
+						$this->usage['completionTokens'] += $response['response']['usage']['completionTokens'];
+					if (isset($response['response']['usage']['totalTokens']))
+						$this->usage['totalTokens'] += $response['response']['usage']['totalTokens'];
+				}
+			}
+			echo ("\rPlaying Devil's Advocate...($response_count/$response_total)...");
+		}
+		echo ("done.\n");
+		$messages[] = ["role" => "user", "content" => "Based on the User Input and Possible Responses, and while mitigating the issues identified by the Devil's Advocates,\n" .
+			"Use your outside-the-box Critical Thinking skills to come up with the most accurate and concise response.\n" .
+			"Give only your final response without any cognitive distortions."];
+		$messagess = [];
+		$messagess[] = $messages;
+		echo ("Resolving Final Response...(0/1)...");
+		$responses = $bunnyai->get($bunnyai->build_prompts($messagess));
+		$response_count = 0;
+		$response_total = count($responses);
+		$messages = $base_input;
+		foreach ($responses as $response) {
+			$response_count++;
+			if (isset($response['response'])) {
+				if (isset($response['response']['choices'])) {
+					foreach ($response['response']['choices'] as $choice) {
+						if (isset($choice['message']) && isset($choice['message']['content'])) {
+							$final_response = $choice['message']['content'];
+						}
+					}
+				}
+				if (isset($response['response']['usage'])) {
+					if (isset($response['response']['usage']['promptTokens']))
+						$this->usage['promptTokens'] += $response['response']['usage']['promptTokens'];
+					if (isset($response['response']['usage']['completionTokens']))
+						$this->usage['completionTokens'] += $response['response']['usage']['completionTokens'];
+					if (isset($response['response']['usage']['totalTokens']))
+						$this->usage['totalTokens'] += $response['response']['usage']['totalTokens'];
+				}
+			}
+			echo ("\rResolving Final Response...($response_count/$response_total)...");
+		}
+		echo ("done.\n");
 		print_r($responses);
-		print_r($messages);
+		echo ("Final Response: $final_response\n");
 		print_r($this->usage);
 	}
 }
